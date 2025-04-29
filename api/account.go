281920808b0d0f22ff226761b62d3d2018e130a5
Ohlulu Hsih <z30262226@gmail.com>
2025-04-29 17:07:35 +0800

    feat: implement account creation API

--- api/account.go
+++ api/account.go
@@ -20,6 +20,20 @@ func (server *Server) createAccount(ctx *gin.Context) {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
+
+	arg := db.CreateAccountParams{
+		Owner:    req.Owner,
+		Balance:  0,
+		Currency: req.Currency,
+	}
+
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
 }
 
 type getAccountRequest struct {
